21744e2deb697530ef53ab83ebaafafc24795dfa
Sergey <neftianik@inbox.ru>
2023-05-24 23:26:02 +0300

    Create filters, news feed

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_share(request, post_id):
@@ -37,24 +39,29 @@ def post_share(request, post_id):
                                                     'sent': sent})
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     # Постраничная разбивка
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
-    # Если page_number не целое число, то
-    # выдать первую страницу
+        # Если page_number не целое число, то
+        # выдать первую страницу
         posts = paginator.page(1)
     except EmptyPage:
-    # Если page_number находится вне диапазона, то
-    # выдать последнюю страницу
+        # Если page_number находится вне диапазона, то
+        # выдать последнюю страницу
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):  # arguments
@@ -68,11 +75,20 @@ def post_detail(request, year, month, day, post):  # arguments
     comments = post.comments.filter(active=True)
     # Форма для комментирования пользователями
     form = CommentForm()
+
+    # Список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+        .order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
@@ -102,7 +118,6 @@ def post_comment(request, post_id):
         # Сохранить комментарий в базе данных
         comment.save()
     return render(request, 'blog/post/comment.html',
-                            {'post': post,
-                             'form': form,
-                             'comment': comment})
-
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
